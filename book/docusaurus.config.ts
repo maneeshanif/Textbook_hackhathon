863d0c865b472c9e72d022948f162a75e031c636
maneeshanif <manees7865@gmail.com>
2025-12-06 23:53:40 +0500

    baseurl of docusarus config corrected

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   url: 'https://maneeshanif.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Textbook_hackhathon/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'maneeshanif',
